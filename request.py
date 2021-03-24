7cc62d681c7a2415afa2bbe82f741330d4d83bba
Nate Singer <nathaniel@singer.cloud>
2021-03-23 23:57:24 -0500

    beta final v0.2b

--- request.py
+++ request.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import requests
-import json
-import urllib
-import os
-import platform
 import io
-import warnings
-from progress.bar import Bar
+import json
+import os
 from pathlib import Path
-
+import platform
+from progress.bar import Bar
+import requests
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename, askdirectory
+import urllib
+import warnings
 warnings.filterwarnings("ignore")
 
 class FuckPic:
@@ -23,7 +24,7 @@ class FuckPic:
         self.image_generate = None
 
         self.proxy = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0', 'Accept': 'application/json, text/plain, */*', 'Authorization':jwt_auth}
+        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0', 'Accept': 'application/json, text/plain, */*', 'Authorization':jwt}
 
         self.upload_image()
         self.generate_image()
@@ -50,7 +51,7 @@ class FuckPic:
     def generate_image(self):
         """"""
         try:
-            file = {'input': open('.tmpfile.png','rb')}
+            file = {'input': open('.tmpfile.png','rb'), 'custom_style': open('/home/singern/Desktop/0109.png','rb')}
             form_data = {'custom_style': '', 'style':3, 'genre':1}
 
             if self.proxy_state:
@@ -65,24 +66,38 @@ class FuckPic:
             print(f"\nInvalid server response:{r.content}")
             exit()
 
-#INIT
-project_id = "12193"
-jwt_auth = "JWT eyJhbGciOiJSUzI1NiIsImtpZCI6IjRlMDBlOGZlNWYyYzg4Y2YwYzcwNDRmMzA3ZjdlNzM5Nzg4ZTRmMWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcGxheWZvcm0tdXNlcnMtcHJvZCIsImF1ZCI6InBsYXlmb3JtLXVzZXJzLXByb2QiLCJhdXRoX3RpbWUiOjE2MTYzODA0NzgsInVzZXJfaWQiOiJLSUVHcmROSEZ5VnoyV1ZKRzdZSkFod0R2Z28xIiwic3ViIjoiS0lFR3JkTkhGeVZ6MldWSkc3WUpBaHdEdmdvMSIsImlhdCI6MTYxNjM5NDU3OCwiZXhwIjoxNjE2Mzk4MTc4LCJlbWFpbCI6InRlZGR5amh5ZGVAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBob25lX251bWJlciI6IisxOTc4NTAwNTk5MyIsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsicGhvbmUiOlsiKzE5Nzg1MDA1OTkzIl0sImVtYWlsIjpbInRlZGR5amh5ZGVAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.Z_LAp2n9S9oWso9Vw31AMpjot5L6Kp9zbEwFU8EhyiL-S6VJM7ka7dEEE3KhuHHMdAcppf8pTIFJ3S72XxjgSv1aBDd4u05pm1tFNLv1ICJ2i8iIQ4PU19NprZ6jjvAExV7aFF4REXK8Jdtz44B73kIENNmoGD-nFTmSgTvPQSYM88dbQmjO0Vcy7DKLER_GRisPdGDW9cDA8IjJvIib9eOd6YURUazSupCHB4oFOSOVV60JFt-c2EFhxrtd2IHHXqvz8HU1Lku0artaTOB8C3B3q_ahO-giJITmyqYwZ45qwEYN-J2jx73KYg9pntrUn1QvBbQj9lgaUMsRtGWUHQ"
+def get_user_input():
+    #https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 
+    print("Create a project, save the template file on your local machine...")
+    input("Hit [enter] to proceed with selecting it...")
+    file_template = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    input("Hit [enter] to proceed with selecting a directory of frames...")
+    directory_frame = askdirectory()
+    input("Hit [enter] to proceed with selecting an output directory...")
+    directory_output = askdirectory()
+    project_id = input("Please provde a project ID from the URL of the project page, as an integer: ")
+    jwt = input("Please provide the authorization header(JWT) like \"JWT eyJh...\": ")
+
+    return file_template, directory_frame, directory_output, project_id, jwt
+
+#INIT
 try:
-    #frames_dir = Path(input("Specify an absolute path to a directory of video frames: "))
-    frames_dir = Path("/home/singern/Desktop/illegal_shit/frames")
-    if not frames_dir.is_dir():
+    file_template, directory_frame, directory_output, project_id, jwt = get_user_input()
+
+    directory_frame = Path("/home/singern/Desktop/illegal_shit/frames")
+    if not directory_frame.is_dir():
         print("You suck, give me a real folder... or check permissions??")
         exit()
 
-    files = [f for f in os.listdir(frames_dir)]
+    files = [f for f in os.listdir(directory_frame)]
     files.sort()
 
     if platform.system() == "Windows":
-        files_input = [f"{frames_dir.resolve()}\{f}" for f in files]
+        files_input = [f"{directory_frame.resolve()}\{f}" for f in files]
     else: #its linux or macos
-        files_input = [f"{frames_dir.resolve()}/{f}" for f in files]
+        files_input = [f"{directory_frame.resolve()}/{f}" for f in files]
 
     save_dir = os.path.dirname(os.path.abspath(__file__)) + ""
     if platform.system() == "Windows":
@@ -99,7 +114,7 @@ try:
     with Bar("Processing frames") as bar:
         bar.max = total_file_count
         for i,f in enumerate(files_input):
-            new_picture = FuckPic(f, project_id, jwt_auth, True)
+            new_picture = FuckPic(f, project_id, jwt, True)
             with open(files_output[i], 'wb') as io:
                 io.write(new_picture.image_generate)
             bar.next()
